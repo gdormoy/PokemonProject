94fdef6c64bbec8467b7a63c5ace7ce8c1610df3
gdormoy <dormoy.guillaume@gmail.com>
2020-03-30 09:52:24 +0200

    first commit

---
+++ models/Competence.js
@@ -0,0 +1,7 @@
+class competence {
+    constructor(nom, typeCompetence, puissance=null) {
+        this.nom = nom
+        this.typeCompetence = typeCompetence
+        this.puissance = puissance
+    }
+}
